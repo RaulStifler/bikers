64674ba148f32b911fd143344e1c1488a31e23d3
RaulStifler <raulstifler25@gmail.com>
2020-05-02 03:14:30 -0500

    se implementa la libreria scroll reveal

--- menu.js
+++ menu.js
@@ -1,3 +1,10 @@
 document.querySelector('.menu-btn').addEventListener('click',() => {
     document.querySelector('.nav-menu').classList.toggle('show-menu');
 });
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.cards-container .first', { delay: 200 });
+ScrollReveal().reveal('.cards-container .second', { delay: 400 });
+ScrollReveal().reveal('.cards-container .third', { delay: 600 });
+ScrollReveal().reveal('.cards-container .fourth', { delay: 800 });
+ScrollReveal().reveal('.banner', { delay: 250 });
